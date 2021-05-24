829ae81f19d809866f9305cad0f03504839c6e9f
mireu-san <67046863+mireu-san@users.noreply.github.com>
2021-05-24 20:18:00 +0900

    Header complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Header from "./Components/Containers/Header/Header";
 
 
 function App() {
   return (
     <div className="App">
-
+      <Header />
     </div>
   );
 }
---
+++ src/Components/Pages/Contact/Contact.jsx
@@ -0,0 +1,10 @@
+import "./Contact.scss";
+
+
+export default function Contact() {
+    return (
+        <div className="contact">
+            
+        </div>
+    )
+}
