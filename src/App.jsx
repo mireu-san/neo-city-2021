da88ea25c8a3c134304c68531ab7b11fc64aced6
mireu-san <67046863+mireu-san@users.noreply.github.com>
2021-05-25 21:26:12 +0900

    vertical scrolling + each page combination - app.scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import Header from "./Components/Containers/Header/Header";
+import Footer from "./Components/Containers/Footer/Footer";
 
+import Home from "./Components/Pages/Home/Home";
+import Portfolio from "./Components/Pages/Portfolio/Portfolio";
+import Contact from "./Components/Pages/Contact/Contact";
+
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Header />
+      <Footer />
+      <div className="sections">
+        
+        <Home />
+        <Portfolio />
+        <Contact />
+      </div>
     </div>
   );
 }
--- src/Components/Pages/Portfolio/Portfolio.jsx
+++ src/Components/Pages/Portfolio/Portfolio.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import "./Portfolio.scss";
 
 export default function Portfolio() {
     return (
