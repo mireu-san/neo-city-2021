b6c2d654a696b0ede102eeb7b7d8024a15714888
mireu-san <67046863+mireu-san@users.noreply.github.com>
2021-05-26 23:28:53 +0900

    locations are now stable - header

--- src/Components/Containers/Header/Header.jsx
+++ src/Components/Containers/Header/Header.jsx
@@ -3,7 +3,12 @@ import "./Header.scss";
 export default function Header() {
     return (
         <div className="header">
-            
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#home" className="logo">Logo location</a>
+                </div>
+                <div className="right">right side section, header</div>
+            </div>
         </div>
     )
 }
