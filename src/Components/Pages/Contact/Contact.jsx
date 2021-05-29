0f229d126121c797d49e0d0f12f04d5c26590eac
mireu-san <67046863+mireu-san@users.noreply.github.com>
2021-05-30 00:55:35 +0900

    contact - 1) minor error , UI box issue 2) final safe zone

--- src/Components/Pages/Contact/Contact.jsx
+++ src/Components/Pages/Contact/Contact.jsx
@@ -1,9 +1,19 @@
 import "./Contact.scss";
 
-
 export default function Contact() {
     return (
         <div className="contact" id="contact">
+            <div className="left">
+                <img src="assets/email.jpg" alt=""/>
+            </div>
+            <div className="right">
+                <h2>Contact</h2>
+                <form>
+                    <input type="text" placeholder="Email"/>
+                    <textarea placeholder="Message"></textarea>
+                    <button type="submit">Send</button>
+                </form>
+            </div>
             
         </div>
     )
