85835ee3bd8cc6960ca6c702099db0e6c2aa66ad
mireu-san <67046863+mireu-san@users.noreply.github.com>
2021-05-31 11:31:44 +0900

    contact - last safe version before editing contact.scss

--- src/Components/Pages/Contact/Contact.jsx
+++ src/Components/Pages/Contact/Contact.jsx
@@ -1,13 +1,13 @@
-import "./Contact.scss";
 import { useState } from "react";
+import "./Contact.scss";
 
 export default function Contact() {
-    const [ message, setMessage ] = useState(false)
+    const [ message, setMessage ] = useState(false);
 
     const handleSubmit = (e) => {
         e.preventDefault();
         setMessage(true)
-    }
+    };
 
     return (
         <div className="contact" id="contact">
@@ -17,13 +17,13 @@ export default function Contact() {
             <div className="right">
                 <h2>Contact</h2>
                 <form onSubmit={handleSubmit}>
-                    <input type="text" placeholder="Email"/>
+                    <input type="text" placeholder="Email" />
                     <textarea placeholder="Message"></textarea>
                     <button type="submit">Send</button>
-                    {message && <span>감사합니다! 확인 후 답장드리겠습니다.</span>}
+                    {message && <span>Thank you. 확인 후 답장드리겠습니다.</span>}
                 </form>
             </div>
             
         </div>
-    )
+    );
 }
